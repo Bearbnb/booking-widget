78576a63be58cb84c85b9600e82e39b419f887ed
Andrew Ngo <andrew@Andrews-MacBook-Pro.local>
2018-09-07 18:16:23 -0700

    Includes data seed file

--- database/index.js
+++ database/index.js
@@ -70,57 +70,11 @@ const insertBookingData = (bookingData, callback) => {
 }
 
 
-
+module.exports.connection = connection;
 module.exports.findAvailableDates = findAvailableDates;
 module.exports.findHouseData = findHouseData;
-
-// Inserts Calendar Filler Data***
-
-// const calendarData = [];
-//
-// for (let i = 1; i < 100; i++) { // Iterates through 100 houses
-//   for (let j = 0; j < 365; j++) { // Iterates through one year
-//     calendarData.push([
-//       moment('2018-01-01').add(j, 'day').format('YYYY-MM-DD'), // Booking_date
-//       i, // House_id
-//       Math.random(), // Occupany (0 = false, 1 = true)
-//       Math.random() * 560, // Price
-//     ]);
-//   }
-// }
-//
-// connection.query('INSERT INTO calendar (booking_date, house_id, availability, price) VALUES ?', [calendarData], (error, results) => {
-//   if (error) {
-//     console.log('ERROR calendarData insert to db failed');
-//   } else {
-//     console.log('calendarData insert to db successful!');
-//     console.log(results);
-//   }
-// });
-
-// Inserts Houses Filler Data***
-
-// const houseData = [];
-//
-// for (let i = 0; i < 100; i++) {
-//   houseData.push([
-//     Math.random() * 5, // average_rating
-//     Math.random() * 250, // ratings
-//     Math.random() * 170, // cleaning_fee
-//     Math.random() * 120, // service_fee
-//     Math.random() * 12, // occupancy
-//   ]);
-// }
-//
-// connection.query('INSERT INTO houses (average_rating, ratings, cleaning_fee, service_fee, occupancy) VALUES ?', [houseData], (error, results) => {
-//   if (error) {
-//     console.log('ERROR insert to db failed');
-//   } else {
-//     console.log('Insert to db successful!');
-//     console.log(results);
-//   }
-// });
-
+module.exports.findHousePrice = findHousePrice;
+module.exports.insertBookingData = insertBookingData;
 
 // Test
 
@@ -150,7 +104,7 @@ module.exports.findHouseData = findHouseData;
 // })
 
 // const bookingData = {
-//   house_id: 98, check_in: '2018-01-01', check_out: '2018-01-02', adults: 2, children: 2, infants: 0, total_occupancy: 4, price: 40, cleaning_fee: 67, service_fee: 20, total_price: 127,
+//   house_id: 99, check_in: '2018-01-01', check_out: '2018-01-02', adults: 2, children: 2, infants: 0, total_occupancy: 4, price: 40, cleaning_fee: 67, service_fee: 20, total_price: 127,
 // };
 //
 // insertBookingData(bookingData, (error, results) => {
