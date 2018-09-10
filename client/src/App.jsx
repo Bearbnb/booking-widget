227085ae505724df422531f632f5c17a5c3710dd
Andrew Ngo <andrew@Andrews-MacBook-Pro.local>
2018-09-10 14:29:13 -0700

    Connected client with server and database

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Calendar from './Calendar.jsx'
+import $ from "jquery";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      check_in: '',
+      check_out: '',
+      adults: '',
+      children: '',
+      infants: '',
+      totalGuests: '',
+      rates: '',
+    }
+  }
+
+  getHouseData () {
+    $.ajax({
+      method: 'GET',
+      url: '/houses/1',
+      success: (results) => {
+        console.log('GET request called!')
+        console.log(results)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+  componentDidMount () {
+    this.getHouseData();
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from react
+        <br />
+        <input type="submit"/>
+        <Calendar />
+      </div>
+    )
+  }
+}
+
+export default App;
